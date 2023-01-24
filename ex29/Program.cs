accad3708e14d78f3e2699bc63625f43a43df249
Alexey <alexragnarek@gmail.com>
2023-01-24 22:44:26 +0500

    ex 29 is complited

---
+++ ex29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+;
+int [] array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+  Console.Write($"Insert {i + 1} number: ");
+  array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
