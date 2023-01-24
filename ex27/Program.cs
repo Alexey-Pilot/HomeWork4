86da663c29d20f217642e02ae25b1b2aa48060b1
Alexey <alexragnarek@gmail.com>
2023-01-24 22:35:30 +0500

    ex 27 is complited

---
+++ ex27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Insert the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+while (num > 0)
+{
+  summ += num%10;
+  num /= 10;
+}
+Console.WriteLine($"Summ of the digits equals to {summ}");
