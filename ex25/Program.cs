7d93bddbffa0ece23ee58ff9bd465764b1e6fa83
Alexey <alexragnarek@gmail.com>
2023-01-24 22:26:13 +0500

    ex 25 is complited

--- ex25/Program.cs
+++ ex25/Program.cs
@@ -1,9 +1,4 @@
 ﻿Console.Clear();
-// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// Math.Pow(ЗАПРЕЩЕНО)
-// 3, 5 -> 243 (3⁵)
-
-// 2, 4 -> 16
 Console.Write("Insert number A: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Insert number B: ");
