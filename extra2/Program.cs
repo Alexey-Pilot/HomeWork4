b65fab181536d01b8bbf4c90063be011b71bdb16
Alexey <alexragnarek@gmail.com>
2023-01-25 13:18:20 +0500

    extra 2 is complited

---
+++ extra2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Введиле четное число от 4 до 998: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num % 2 == 1 || num < 4 || num > 998) // вводим число подходящее под условия задачи
+{ 
+  Console.Write("Введиле четное число от 4 до 998: ");
+  num = Convert.ToInt32(Console.ReadLine());
+}
+bool isSimple(int n) // определяем простое число или нет
+{
+  for (int i = 2; i < n; i++)
+  {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+int s = new Random().Next(4, num);
+do
+{
+  s = new Random().Next(4, num);
+}while(isSimple(s) == false || isSimple(num - s) == false);
+
+
+Console.WriteLine($"{s}, {num - s}");
